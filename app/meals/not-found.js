4ce4096b2af344f3f039fe799739024692747c99
Wilberforce <wmaggickk@gmail.com>
2024-06-20 11:38:06 +0000

    continued exploring nextjs

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Meal not found</h1>
+            <p>Unfortunately, we could nt find the requested page or meal data.</p>
+        </main>
+    )
+}
